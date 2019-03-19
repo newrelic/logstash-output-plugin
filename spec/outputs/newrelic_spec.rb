a3bcdbcdd4da2ac0b7314171d05af303972eb347
Brian McFeely <bmcfeely@newrelic.com>
2019-03-19 13:52:23 -0700

    try to parse body, does not work yet

--- spec/outputs/newrelic_spec.rb
+++ spec/outputs/newrelic_spec.rb
@@ -4,6 +4,7 @@ require "logstash/outputs/newrelic"
 require "logstash/codecs/plain"
 require "logstash/event"
 require "webmock/rspec"
+require "zlib"
 
 describe LogStash::Outputs::Newrelic do
   let (:api_key) { "someAccountKey" }
@@ -17,6 +18,12 @@ describe LogStash::Outputs::Newrelic do
     }
   }
 
+  def gunzip(bytes)
+    # Zlib::Inflate.inflate(bytes)
+    gz = Zlib::GzipReader.new(StringIO.new(bytes))
+    gz.read
+  end
+
   before {
     @newrelic_output = LogStash::Plugin.lookup("output", "newrelic").new(simple_config)
     @newrelic_output.register
@@ -29,20 +36,18 @@ describe LogStash::Outputs::Newrelic do
   context "validation of config" do
     it "requires api_key" do
       no_api_key_config = {
-        "account_id" => account_id
+        "account_id" => account_id,
       }
 
-      expect {LogStash::Plugin.lookup("output", "newrelic").new(no_api_key_config)}
-        .to raise_error LogStash::ConfigurationError
+      expect { LogStash::Plugin.lookup("output", "newrelic").new(no_api_key_config) }.to raise_error LogStash::ConfigurationError
     end
 
     it "requires account_id" do
       no_account_id_config = {
-        "api_key" => api_key
+        "api_key" => api_key,
       }
 
-      expect {LogStash::Plugin.lookup("output", "newrelic").new(no_account_id_config)}
-        .to raise_error LogStash::ConfigurationError
+      expect { LogStash::Plugin.lookup("output", "newrelic").new(no_account_id_config) }.to raise_error LogStash::ConfigurationError
     end
   end
 
@@ -53,13 +58,33 @@ describe LogStash::Outputs::Newrelic do
       event = LogStash::Event.new({ "message" => "Test message" })
       @newrelic_output.multi_receive([event])
 
-      wait_for(a_request(:post, base_uri).with(
-        headers: {
-          'X-Insert-Key' => api_key,
-          'X-Event-Source' => 'logs',
-          'Content-Encoding' => 'gzip'})).to have_been_made
+      wait_for(a_request(:post, base_uri)
+        .with(headers: {
+                "X-Insert-Key" => api_key,
+                "X-Event-Source" => "logs",
+                "Content-Encoding" => "gzip",
+              })).to have_been_made
     end
   end
+
+  ###########################################################################
+  # Doesn't  currently work -- need to use hash_including
+  ###########################################################################
+  # context "request body" do
+  #   it "single event with 'message' field" do
+  #     stub_request(:any, base_uri).to_return(status: 200)
+
+  #     event = LogStash::Event.new({ :message => "Test message" })
+  #     @newrelic_output.multi_receive([event])
+
+  #     wait_for(a_request(:post, base_uri)
+  #       .with { |req|
+  #       # puts gunzip(req.body)
+  #       # puts JSON.parse(gunzip(req.body))
+  #       JSON.parse(gunzip(req.body)) == { :message => "Test message" }
+  #     }).to have_been_made
+  #   end
+  # end
 
   context "request body" do
     it "makes POST call to collector" do
