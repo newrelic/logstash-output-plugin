a642bc1cbfa3b950d9f37557b0ca4c9591bf05c1
Brian McFeely <bmcfeely@newrelic.com>
2019-03-19 08:49:02 -0700

    multiple events

--- spec/outputs/newrelic_spec.rb
+++ spec/outputs/newrelic_spec.rb
@@ -6,36 +6,48 @@ require "logstash/event"
 require "webmock/rspec"
 
 describe LogStash::Outputs::Newrelic do
-
-  let (:api_key) { 'someAccountKey' }
-  let (:account_id) { '123' }
-  let (:base_uri) { 'https://testing-example-collector.com' }
+  let (:api_key) { "someAccountKey" }
+  let (:account_id) { "123" }
+  let (:base_uri) { "https://testing-example-collector.com" }
   let (:simple_config) {
     {
-      'api_key' => api_key,
-      'account_id' => account_id,
-      'base_uri' => base_uri
+      "api_key" => api_key,
+      "account_id" => account_id,
+      "base_uri" => base_uri,
     }
   }
 
-   before { 
-     @newrelic_output = LogStash::Plugin.lookup('output', 'newrelic').new(simple_config)
-     @newrelic_output.register
-   }
+  before {
+    @newrelic_output = LogStash::Plugin.lookup("output", "newrelic").new(simple_config)
+    @newrelic_output.register
+  }
 
-   after { 
-     @newrelic_output&.shutdown
-   }
+  after {
+    @newrelic_output&.shutdown
+  }
 
-  context 'simple event' do
-      it 'makes POST call to collector' do
-          stub_request(:any, base_uri).
-            to_return(status: 200)
+  context "single event" do
+    it "makes POST call to collector" do
+      stub_request(:any, base_uri).
+        to_return(status: 200)
 
-          event = LogStash::Event.new({'message' => 'Test message'})
-          @newrelic_output.multi_receive([event])
+      event = LogStash::Event.new({ "message" => "Test message" })
+      @newrelic_output.multi_receive([event])
 
-          wait_for(a_request(:post, base_uri)).to have_been_made
-      end
+      wait_for(a_request(:post, base_uri)).to have_been_made
+    end
+  end
+
+  context "multiple events" do
+    it "makes POST call to collector" do
+      stub_request(:any, base_uri).
+        to_return(status: 200)
+
+      event1 = LogStash::Event.new({ "message" => "Test message 1" })
+      event2 = LogStash::Event.new({ "message" => "Test message 2" })
+      @newrelic_output.multi_receive([event1, event2])
+
+      wait_for(a_request(:post, base_uri)).to have_been_made
+    end
   end
 end
