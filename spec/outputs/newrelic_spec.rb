43f078fb2938197bfe47d3ce019514395d1fe9a4
Brian McFeely <bmcfeely@newrelic.com>
2019-03-19 08:26:56 -0700

    validate mock call made (with sleep)

--- spec/outputs/newrelic_spec.rb
+++ spec/outputs/newrelic_spec.rb
@@ -3,13 +3,14 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/newrelic"
 require "logstash/codecs/plain"
 require "logstash/event"
+require "webmock/rspec"
 
 
 describe LogStash::Outputs::Newrelic do
 
   let (:api_key) { 'lH7pIMEpl5oCbNs8g1jTN4VllAITqtxQ' }
   let (:account_id) { '756053' }
-  let (:base_uri) { 'https://staging-insights-collector.newrelic.com/v1/accounts/' }
+  let (:base_uri) { 'https://testing-insights-collector.newrelic.com' }
 
   let (:simple_config) {
     {
@@ -23,17 +24,22 @@ describe LogStash::Outputs::Newrelic do
   let (:event) {
     LogStash::Event.new(
     {
-      'message' => 'Test message - Brian4567890'
+      'message' => 'Test message - Brian456789098999'
     })
    }
 
   context 'simple call' do
       it 'does not blow up' do
+          stub_request(:any, base_uri).
+            to_return(body: "abc", status: 500)
+
           newrelic_output = LogStash::Plugin.lookup('output', 'newrelic')
                   .new(simple_config)
           newrelic_output.register
           newrelic_output.multi_receive([event])
           newrelic_output.shutdown
+          sleep 5
+          expect(a_request(:post, base_uri)).to have_been_made
       end
   end
 end
