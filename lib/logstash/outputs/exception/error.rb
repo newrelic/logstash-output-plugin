02c46549f8e70731723ad08cb446a0d93b83b601
tejunior <te_junior@yahoo.com.br>
2021-02-23 18:51:52 +0100

    Missing attribute

--- lib/logstash/outputs/exception/error.rb
+++ lib/logstash/outputs/exception/error.rb
@@ -1,6 +1,6 @@
 class Error
   class BadResponseCodeError < StandardError
-    attr_reader :url, :response_code, :response_body
+    attr_reader :url, :response_code
 
     def initialize(response_code, url)
       @response_code = response_code
